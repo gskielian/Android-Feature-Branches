145a88f05bf20d853970621a6bdf77f0d64a95d1
gskielian <gregory.kielian@gmail.com>
2015-07-29 22:44:44 -0700

    button opens a second page/activity

--- app/src/main/java/org/foodrev/www/android_feature_branches/Main2Activity.java
+++ app/src/main/java/org/foodrev/www/android_feature_branches/Main2Activity.java
@@ -1,45 +1,22 @@
 package org.foodrev.www.android_feature_branches;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-    private Button basicButton;
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        basicButton = (Button) findViewById(R.id.button);
-
-
-        basicButton.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View view) {
-                //place button action here
-                openNextActivity();
-            }
-        });
-
-
-    }
-
-    private void openNextActivity(){
-        // Intent intent = new Intent(<This Activity's Name>.this, <Next Activity's Name>.class)
-        Intent intent = new Intent(MainActivity.this, Main2Activity.class);
-        startActivity(intent);
+        setContentView(R.layout.activity_main2);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main2, menu);
         return true;
     }
 
