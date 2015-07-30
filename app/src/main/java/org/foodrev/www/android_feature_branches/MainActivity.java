f576d0066731345cd1015cf95bec2a8c551d5b78
gskielian <gregory.kielian@gmail.com>
2015-07-29 22:08:37 -0700

    button triggers notification

--- app/src/main/java/org/foodrev/www/android_feature_branches/MainActivity.java
+++ app/src/main/java/org/foodrev/www/android_feature_branches/MainActivity.java
@@ -1,15 +1,20 @@
 package org.foodrev.www.android_feature_branches;
 
+import android.app.NotificationManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.NotificationCompat;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     private Button basicButton;
+
+    private NotificationCompat.Builder mBuilder;
+    private NotificationManager mNotifyMgr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +27,28 @@ public class MainActivity extends AppCompatActivity {
         basicButton.setOnClickListener(new View.OnClickListener() {
             public void onClick(View view) {
                 //place button action here
+                makeNotification();
             }
         });
 
 
+    }
+    private void makeNotification() {
+        //set up the notification details
+        mBuilder =
+                (NotificationCompat.Builder) new NotificationCompat.Builder(getApplicationContext())
+                        .setSmallIcon(R.drawable.notification_template_icon_bg)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+        int mNotificationId = 001;
+
+        // Gets a NotificationManager service instance
+        mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+
+        // make notification.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
     }
 
     @Override
